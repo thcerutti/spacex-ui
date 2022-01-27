c617fae1020a01f8dbc9a19ed0a51b219f73059f
Thiago Cerutti <thiagocerutti@gmail.com>
2022-01-26 23:52:24 -0300

    adiciona Google Analytics ID

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-178486718-1',
+  router
+});
 
 new Vue({
   router,
