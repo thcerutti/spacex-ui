94a78f5fe8fd5f51142aa540a26317730caedb51
Thiago Cerutti <thiagocerutti@gmail.com>
2022-01-27 00:32:58 -0300

    incluir HotJar tracker

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueAnalytics from 'vue-analytics';
+import Hotjar from 'vue-hotjar'
 
 Vue.config.productionTip = false
 
@@ -9,6 +10,10 @@ Vue.use(VueAnalytics, {
   id: 'UA-178486718-1',
   router
 });
+
+Vue.use (Hotjar, {
+  id: '2802217'
+})
 
 new Vue({
   router,
